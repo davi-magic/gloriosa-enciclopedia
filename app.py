c5ae72060f56af7e4a769beb1861383a7f32e436
davi-magic <davischuinanunes@gmail.com>
2025-04-25 15:08:19 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 
 import streamlit as st
 
-senha = st.text_input("Digite a senha para acessar:", type="password")
+st.set_page_config(page_title="A Gloriosa Enciclopédia", layout="centered")
+senha = st.text_input("Digite a senha para acessar a Gloriosa Enciclopédia:", type="password")
 
-if senha == "botaelasil":
-    st.title("A Gloriosa Enciclopédia")
-    st.write("Aqui você poderá interagir com sua inteligência artificial.")
-else:
+if senha != "botaelasil":
     st.warning("Acesso restrito. Digite a senha correta.")
+    st.stop()
+
+st.title("A Gloriosa Enciclopédia")
+st.write("Versão preliminar em funcionamento.")
+st.write("Em breve você poderá fazer perguntas sobre campeonatos, jogadores e previsões.")
