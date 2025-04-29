6725e5062f1fbf002524036c4642be1a803bfa61
davi-magic <davischuinanunes@gmail.com>
2025-04-29 15:53:21 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-import streamlit as st
-from modules.login import login
-from modules.parser import parse_data
-from modules.qa import answer_question
-
-st.set_page_config(page_title="Gloriosa Enciclopédia", layout="centered")
-if login():
-    st.title("A Gloriosa Enciclopédia")
-    uploaded = st.file_uploader("Envie o campeonato (.json):")
-    if uploaded:
-        data = parse_data(uploaded)
-        question = st.text_input("Faça sua pergunta:")
-        if question:
-            answer = answer_question(data, question)
-            st.write(answer)
+# Código principal da Gloriosa Enciclopédia (versão teste 27 abril)
