3ccbc22d450f874d64a8e23d5a76fa7ab7776bef
davi-magic <davischuinanunes@gmail.com>
2025-04-30 23:14:37 -0300

    Delete login.py

--- login.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def login():
-    password = st.text_input("Senha", type="password")
-    if password == "botaelasil":
-        return True
-    elif password:
-        st.error("Senha incorreta.")
-    return False
